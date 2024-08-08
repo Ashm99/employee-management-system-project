ba801f47a44c6a68da43bfee62ea667d2afb1e2a
Ashm99 <ashok.ashm99@gmail.com>
2024-08-08 08:35:22 +0530

    Updated index, view html files and web controller

--- employeemanagementsystem/src/main/java/com/mini_project/employeemanagementsystem/controller/WebController.java
+++ employeemanagementsystem/src/main/java/com/mini_project/employeemanagementsystem/controller/WebController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,15 +23,26 @@ public class WebController {
     public String home(Model model){
         List<EmployeeDTO> employeeDTOList = employeeService.getAllEmployees();
         if(!employeeDTOList.isEmpty()){
-            model.addAttribute("employee-list", employeeDTOList);
+            model.addAttribute("employeeList", employeeDTOList);
+            model.addAttribute("isEmpty", false);
+        }
+        else{
+            model.addAttribute("isEmpty", true);
         }
         return "index";
     }
 
-    // http://localhost:8081/api/employees/home1
-    @GetMapping(value="/home1")
-    public String index(Model model){
-        model.addAttribute("message", "Welcome home");
-        return "index";
+    // http://localhost:8081/api/employees/add-employee
+    @GetMapping(value="/add-employee")
+    public String getEmployee(Model model){
+        return "add-employee";
+    }
+
+    // http://localhost:8081/api/employees/view/{id}
+    @GetMapping(value = "/view/{id}")
+    public String showEmployeeById(@PathVariable(value = "id") Long id, Model model){
+        EmployeeDTO employeeDTO = employeeService.getEmployeeById(id);
+        model.addAttribute("employee", employeeDTO);
+        return "view-employee";
     }
 }
